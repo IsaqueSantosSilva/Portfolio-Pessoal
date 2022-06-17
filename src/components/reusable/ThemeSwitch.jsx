9340b857d0a7d3777025f18ef2fc0f1ff0aaf776
IsaqueSantosSilva <g1isaque.santos@gmail.com>
2022-06-17 02:23:45 -0300

    changes

--- src/components/reusable/ThemeSwitch.jsx
+++ src/components/reusable/ThemeSwitch.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./ThemeSwitch.scss";
 
+import Switch from "react-switch";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
@@ -20,16 +22,23 @@ const disableDarkMode = () => {
 
 const ThemeSwitch = () => {
   const [buttonIncon, setButtonIcon] = useState("fas fa-sun");
+  const [checked, setChecked] = useState(false);
 
+  /**
+   * If the dark mode is not enabled, enable it, set the checkbox to true, and set the button icon to the
+   * moon. Otherwise, disable it, set the checkbox to false, and set the button icon to the sun.
+  */
   const changeTheme = () => {
     darkMode = localStorage.getItem("darkMode");
     window.location.reload(true);
 
     if (darkMode !== "enabled") {
       enableDarkMode();
+      setChecked(true);
       setButtonIcon("fas fa-moon");
     } else {
       disableDarkMode();
+      setChecked(false);
       setButtonIcon("fas fa-sun");
     }
   };
@@ -37,17 +46,28 @@ const ThemeSwitch = () => {
   useEffect(() => {
     if (darkMode === "enabled") {
       enableDarkMode();
+      setChecked(true);
       setButtonIcon("fas fa-moon");
     } else {
+      setChecked(false);
       setButtonIcon("fas fa-sun");
     }
   }, []);
 
   return (
-    <div>
+    <div className="themeSwitch-container">
       <button className="darkSwitch-btn" onClick={() => changeTheme()}>
         <FontAwesomeIcon className="icon-moon" icon={buttonIncon} />
       </button>
+      <div className="slider">
+        <Switch
+          offColor="#222"
+          height={17}
+          width={40}
+          onChange={() => changeTheme()}
+          checked={checked}
+        />
+      </div>
     </div>
   );
 };
